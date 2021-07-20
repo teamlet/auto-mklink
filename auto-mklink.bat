6377835aa6b90a639439918fdaca8b6ffdec80df
teamlet <david@Soachina.org>
2021-07-20 22:52:03 +0800

    Update auto-mklink.bat
    修改错误花括号的Bug；增加了对目标文件的判断，防止重复覆盖。

--- auto-mklink.bat
+++ auto-mklink.bat
@@ -1,24 +1,27 @@
 @echo off
 :: creator:teamlet
 :: date:2021-07-20
-
+:: version 1.0.2
 ::===================================================================
-:: 第一处修改：修改目标文件夹路径，结尾不要加斜杠。
 set destFolder=D:/dev-envs
 
-if not exist %destFolder% md %destFolder%
-
 ::===================================================================
-:: 第二处修改：根据实际情况修改数组长度和移动的文件夹名称，结尾需要加斜杠。
-SET objLength=6
-SET Obj[0]=.gradle\
-SET Obj[1]=go\
-SET Obj[2]=.android\
+SET objLength=8
+SET Obj[0]=.android\
+SET Obj[1]=.cargo\
+SET Obj[2]=.gradle\
 SET Obj[3]=.m2\
-SET Obj[4]=.vscode\
-SET Obj[5]=.rustup\
+SET Obj[4]=.rustup\
+SET Obj[5]=.vscode\
+SET Obj[6]=go\
+:: visual studio folder
+SET Obj[7]=source\
 
 ::==================================================================
+if not exist "%destFolder%" (
+     md "%destFolder%"
+     echo "mkdir %destFolder%"
+)
 
 cd %HOMEPATH%
 set objIndex=0
@@ -27,22 +30,24 @@ set objIndex=0
 
 if %objIndex% equ %objLength% goto end
 
-set objCurrent=.rustup
+set objCurrent=0
 
 for /f "usebackq delims== tokens=1-2" %%i in (`set obj[%objIndex%]`) do (
     set objCurrent=%%j
 )
+echo "Current directory ==> %CD%/%objCurrent%"
 
-set CURRENT_FOLDER="%CD%\%objCurrent%"
-echo Current directory ==> %CURRENT_FOLDER%
-
-if exist "%CURRENT_FOLDER%"{
-    xcopy /s /e /q "%CURRENT_FOLDER%" "%destFolder%/%objCurrent%"
-    rd /s /q "%CURRENT_FOLDER%"
-    mklink /j "%CURRENT_FOLDER%" "%destFolder%/%objCurrent%"
-} else{
-    echo Current directory --> %CURRENT_FOLDER% is not exist !
-}
+if exist "%CD%/%objCurrent%" (
+    if not exist "%destFolder%/%objCurrent%" (
+        xcopy /s /e /q "%CD%/%objCurrent%" "%destFolder%/%objCurrent%"
+        rd /s /q "%CD%/%objCurrent%"
+        mklink /j "%CD%/%objCurrent%" "%destFolder%/%objCurrent%"
+    ) else (
+        echo "Target directory --> %destFolder%/%objCurrent% is exist !"
+    )
+) else (
+    echo "Current directory --> %CD%/%objCurrent% is not exist !"
+)
 
 
 set /a objIndex=%objIndex% + 1
